dab8b42c209a995eb97bfdd36abbb5acb5346811
paulamora <moraromeropaula@gmail.com>
2025-11-11 11:08:58 +0100

    RainbowApp1

---
+++ Rainbow/ContentView.swift
@@ -0,0 +1,53 @@
+//
+//  ContentView.swift
+//  Rainbow
+//
+//  Created by Paula Mora Romero on 14/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    //VARIABLES
+    @State private var showAnimation: Bool = false //to start off
+    @State private var textSize = 16.0
+    @State private var text = "Hello Academy"
+    
+    var body: some View {
+        VStack {
+            Toggle(isOn:$showAnimation) {
+                Text("Show Animation")
+            }
+            
+            Spacer()
+            
+            Image(systemName: "rainbow")
+                .font(.system(size: 150))
+                .symbolRenderingMode(.multicolor)
+                .symbolEffect(.variableColor.reversing, isActive: showAnimation) //isActive to control whether it's on or off
+            
+            Text(text) //afegim la variable
+                .font(.system(size: textSize)) //enlloc de posar 16, cridem la variable declarada
+            
+            Spacer()
+            
+            Slider(value: $textSize, in: 16...40) //$ pq llegeix + modifica
+            
+            Button {
+                text = "Hello Learners!"
+            } label: {
+                Text("Change Text")
+            }
+            .controlSize(.large)
+            .buttonStyle(.glassProminent) //liquid glass
+            .padding()
+            
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Rainbow/RainbowApp.swift
@@ -0,0 +1,17 @@
+//
+//  RainbowApp.swift
+//  Rainbow
+//
+//  Created by Paula Mora Romero on 14/10/25.
+//
+
+import SwiftUI
+
+@main
+struct RainbowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
